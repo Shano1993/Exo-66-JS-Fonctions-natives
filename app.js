9b4f527eb85b2bdbc2bb6c03c05f5cc0fdc00cca
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-16 14:16:01 +0100

    js

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log(random());
+
+/*
+    Entier
+ */
+
+let entier = document.getElementById('entier');
+let i = 10;
+
+entier.innerHTML = parseInt(entier.innerHTML) * i;
+console.log(entier.innerHTML);
+
+/*
+    Flottant
+ */
+
+let flottant = document.getElementById('flottant');
+
+flottant.innerHTML = parseFloat(flottant.innerHTML) * i + parseInt(entier.innerHTML);
+console.log(flottant.innerHTML);
+
+/*
+    Non-Numérique
+ */
+
+let nonNumerique = document.getElementById('non-numerique')
+let result = parseInt(nonNumerique.innerHTML);
+
+if(isNaN(nonNumerique.innerHTML)) {
+    nonNumerique.innerHTML = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
